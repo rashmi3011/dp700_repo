add53eb869233039c6f00d1357e7654801b5456c
rashmi3011 <48685736+rashmi3011@users.noreply.github.com>
2025-11-05 21:20:54 +0530

    Committing 1 item from workspace 34f5f735-204d-46fe-8486-66e036327cc6

--- new_folder/Warehouse_lab10.Warehouse/dbo/Tables/DimCustomer.sql
+++ new_folder/Warehouse_lab10.Warehouse/dbo/Tables/DimCustomer.sql
@@ -4,7 +4,6 @@ CREATE TABLE [dbo].[DimCustomer] (
 	[CustomerAltKey] varchar(50) NULL, 
 	[Title] varchar(5) NULL, 
 	[FirstName] varchar(50) NOT NULL, 
-	[LastName] varchar(50) NULL, 
 	[AddressLine1] varchar(200) NULL, 
 	[City] varchar(50) NULL, 
 	[StateProvince] varchar(50) NULL, 
